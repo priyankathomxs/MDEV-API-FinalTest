79024051d26afb7417f13b1b0b9ee7a3096dd8e6
priyankathomxs <200587559@student.georgianc.on.ca>
2024-08-16 03:21:43 -0400

    changed package name

--- Server/Config/stripe.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = {
-    secretKey: process.env.STRIPE_SECRET_KEY,
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY
-};
-//# sourceMappingURL=stripe.js.map
--- Server/Config/stripe.ts
+++
@@ -1,4 +0,0 @@
-export default {
-    secretKey: process.env.STRIPE_SECRET_KEY as string,
-    publishableKey: process.env.STRIPE_PUBLISHABLE_KEY as string
-}
